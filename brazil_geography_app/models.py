44e2821f14408885bc0f17ca9614ed99d6247cbf
tarcisio_souto <tss.labsi@gmail.com>
2023-09-05 02:43:22 -0300

    criando model States

--- brazil_geography_app/models.py
+++ brazil_geography_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class States(models.Model):
+
+    REGION_CHOICES = [('norte','Norte'),
+                      ('nordeste', 'Nordeste'),
+                      ('centro_oeste', 'Centro-Oeste'),
+                      ('sudeste', 'Sudeste'),
+                      ('sul', 'Sul')]
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        null=False,
+        blank=True)
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    population = models.IntegerField(null=False, blank=False)
+    region = models.CharField(choices=REGION_CHOICES, null=False, blank=False, max_length=30)
+
