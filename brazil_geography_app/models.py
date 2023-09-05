bd467fe8fde3f71507bf7c4778541b76506bcdac
tarcisio_souto <tss.labsi@gmail.com>
2023-09-05 03:05:30 -0300

    criando models Counties e Neighborhoods

--- brazil_geography_app/models.py
+++ brazil_geography_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 
+
 class States(models.Model):
 
     REGION_CHOICES = [('norte','Norte'),
@@ -19,3 +20,28 @@ class States(models.Model):
     population = models.IntegerField(null=False, blank=False)
     region = models.CharField(choices=REGION_CHOICES, null=False, blank=False, max_length=30)
 
+
+class Counties(models.Model):
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        null=False,
+        blank=True)
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    population = models.IntegerField(null=False, blank=False)
+    state = models.ForeignKey(States, related_name='counties', on_delete=models.CASCADE)
+
+
+class Neighborhoods(models.Model):
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        null=False,
+        blank=True)
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    population = models.IntegerField(null=False, blank=False, default=0)
+    county = models.ForeignKey(Counties, related_name='neighborhoods', on_delete=models.CASCADE)
